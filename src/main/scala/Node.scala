41eb58d5880f09a3bac581b98d65dc64b3793525
XIANQw <qiwei-xian@qq.com>
2021-02-09 22:43:16 +0100

    Optimize coding style

--- src/main/scala/Node.scala
+++ src/main/scala/Node.scala
@@ -28,29 +28,17 @@ class Node (val id:Int, val terminaux:List[Terminal]) extends Actor {
      var idToindex:Map[Int,Int]= new HashMap[Int, Int]()
      var allNodes:List[ActorSelection] = List()
 
-
-
      def receive = {
-
           // Initialisation
           case Start => {
                displayActor ! Message ("Node " + this.id + " is created")
                checkerActor ! Start
                beatActor ! Start
-
                // Initilisation des autres remote, pour communiquer avec eux
-               
-               var index = 0
-               terminaux.foreach(n => { 
-                    if (n.id != id) {
-                         val remote = context.actorSelection("akka.tcp://LeaderSystem" + n.id + "@" + n.ip + ":" + n.port + "/user/Node")
-                         // Mise a jour de la liste des nodes
-                         this.allNodes = this.allNodes:::List(remote)
-                         idToindex+=(n.id-> index)
-                         index+=1
-                    }
+               terminaux.foreach(n => {
+                    val remote = context.actorSelection("akka.tcp://LeaderSystem" + n.id + "@" + n.ip + ":" + n.port + "/user/Node")
+                    this.allNodes = this.allNodes:::List(remote)
                })
-               println(idToindex)
           }
 
           // Envoi de messages (format texte)
@@ -59,52 +47,33 @@ class Node (val id:Int, val terminaux:List[Terminal]) extends Actor {
           }
 
           case BeatLeader (nodeId) => {
-               this.allNodes.foreach(n => {
-                    n ! IsAliveLeader(nodeId)
-               })
-               checkerActor ! IsAliveLeader(nodeId)
+               allNodes.foreach(actor => actor ! IsAliveLeader(nodeId))
           }
-
           case Beat (nodeId) => {
-               this.allNodes.foreach(n => {
-                    n ! IsAlive(nodeId)
-               })
-               checkerActor ! IsAlive(nodeId)
+               allNodes.foreach(actor => actor ! IsAlive(nodeId))
           }
 
           // Messages venant des autres nodes : pour nous dire qui est encore en vie ou mort
           case IsAlive (id) => {
-               this.checkerActor ! IsAlive(id)
+               checkerActor ! IsAlive(id)
           }
-
           case IsAliveLeader (id) => {
-               this.checkerActor ! IsAliveLeader(id)
+               checkerActor ! IsAliveLeader (id)
           }
 
           // Message indiquant que le leader a change
           case LeaderChanged (nodeId) => {
-               this.beatActor ! LeaderChanged(nodeId)
+               beatActor ! LeaderChanged(nodeId)
           }
-
-          case SendLeaderMessage(msg, dest) =>{
-               println("SendLeaderMessage(" + msg.toString() + ", " + dest + ")")
-               var index:Int = idToindex.getOrElse(dest, 0)
-               this.allNodes(index) ! msg
-
-          }
-          case ALG (list, nodeId) => {
-               println("node: ALG("+nodeId+")")
+          
+          case ALG(list, nodeId) => {
                electionActor ! ALG(list, nodeId)
           }
-          case AVS (list, nodeId) => {
-               println("node: AVS("+nodeId+")")               
-               electionActor ! AVS(list, nodeId)
+          case AVS (list, j) => {
+               electionActor ! AVS(list, j)
           }
-          case AVSRSP(list ,nodeId) => {
-               println("node: AVSRSP("+nodeId+")")
-               electionActor ! AVSRSP(list, nodeId)
-          }
-
+          case AVSRSP (list, k) => {
+               electionActor ! AVSRSP(list, k)
+          } 
      }
-
 }
