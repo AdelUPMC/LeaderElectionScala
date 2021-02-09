ec55432391f77e11bf862a554390d3c2f628fac9
XIANQw <qiwei-xian@qq.com>
2021-02-09 22:41:07 +0100

    Use resolveOne.onComplete to check if the node is alive

--- src/main/scala/CheckerActor.scala
+++ src/main/scala/CheckerActor.scala
@@ -4,7 +4,11 @@ import java.util
 import java.util.Date
 
 import akka.actor._
+import akka.util.Timeout
 
+import scala.util._
+
+import scala.concurrent.Await
 import scala.concurrent.duration._
 import scala.concurrent.ExecutionContext.Implicits.global
 
@@ -20,64 +24,69 @@ class CheckerActor (val id:Int, val terminaux:List[Terminal], electionActor:Acto
      var datesForChecking:List[Date] = List()
      var lastDate:Date = null
 
-     var maxTicks = 30;
-     var curTicks = 0;
      var leader : Int = -1
+     val scheduler = context.system.scheduler
+     var allNodes:List[ActorSelection] = List()
 
      def receive = {
 
           // Initialisation
           case Start => {
+               implicit val timeout = Timeout(5 seconds)
+               terminaux.foreach(n => {
+                    val remote = context.actorSelection("akka.tcp://LeaderSystem" + n.id + "@" + n.ip + ":" + n.port + "/user/Node")
+                    this.allNodes = this.allNodes:::List(remote)
+               })
+               var actorId = 0;
+               for(actorId <- 0 to this.allNodes.length - 1){
+                    this.allNodes(actorId).resolveOne().onComplete{
+                         case Success(act) =>{
+                              if(!this.nodesAlive.contains(actorId))
+                                   this.nodesAlive = this.nodesAlive:::List(actorId)
+                         }
+                         case Failure(ex) =>
+                              this.nodesAlive =  this.nodesAlive.filter(_ != actorId)
+                    }
+               }
+               println(allNodes.toString())
                self ! CheckerTick
           }
 
           // A chaque fois qu'on recoit un Beat : on met a jour la liste des nodes
           case IsAlive (nodeId) => {
-               var contains = false;
-               nodesAlive.foreach(n => {
-                    if(n == nodeId) contains = true;
-               })
-               if(!contains){
+               if (!this.nodesAlive.contains(nodeId))
+               {
                     this.nodesAlive = this.nodesAlive:::List(nodeId)
+                    father ! Message ("Nodes Alive : (" + this.nodesAlive.mkString(", ") + ")")
                }
-               
           }
 
-          case IsAliveLeader (nodeId) => {
-               var contains = false;
-               nodesAlive.foreach(n => {
-                    if(n == nodeId) contains = true;
-               })
-               if(!contains){
-                    this.nodesAlive = this.nodesAlive:::List(nodeId)
-               }
-               this.leader = nodeId;
-          }
+          case IsAliveLeader (nodeId) => this.leader = nodeId
 
-           
           // A chaque fois qu'on recoit un CheckerTick : on verifie qui est mort ou pas
           // Objectif : lancer l'election si le leader est mort
           case CheckerTick => {
-               // Check if this node is leader
-               var contains = this.id == this.leader
-               // Check if leader in list alive
-               if(!contains){
-                    this.nodesAlive.foreach( n=>{
-                         if(n == this.leader) contains = true
-                    })
-               }
-               // We haven't leader's message in maxTicks, we confirm that leader was dead
-               if(curTicks == maxTicks - 1) {
-                    var msg = "leader=" + this.leader + ", nodesAlive: " + nodesAlive.toString()
-                    println(msg);
-                    if(!contains) electionActor ! StartWithNodeList(this.nodesAlive)
-                    this.nodesAlive = List()
-               }
-               curTicks = (curTicks + 1) % maxTicks
-               Thread.sleep(time)
-               self ! CheckerTick
-          }
+               var actorId = 0
+               for(actorId <- 0 to this.allNodes.length - 1) {
+                    if (actorId != id && this.nodesAlive.contains(actorId)) {
+                         implicit val timeout = Timeout(5 seconds)
+                         this.allNodes(actorId).resolveOne().onComplete {                            
+                              case Success(actor) => 
 
+                              case Failure(ex) => {
+                                   this.nodesAlive =  this.nodesAlive.filter(_ != actorId)
+                                   father ! Message ("Leader : "+ this.leader + "; Nodes Alive : (" + this.nodesAlive.mkString(", ") + ")")
+                                   
+                                   if(actorId == leader)
+                                   {
+                                        electionActor ! StartWithNodeList (this.nodesAlive)
+                                   }
+                              }
+                         }    
+                    }
+               }
+               scheduler.scheduleOnce(time milliseconds , self, CheckerTick) 
+          }
      }
 
 
